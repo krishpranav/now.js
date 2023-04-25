09212f82e123651a16130412a9167fab867b1b57
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-25 17:33:13 +0530

    index: create + add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 // main index
+
+const EngineObject = require('./engineobj')
+const point = require('./point')
+
+class Engine {
+    constructor(link, width, height, options = {}) {
+        this._link = link; 
+        this._width = width || window.innerWidth;
+        this._height = height || window.innerHeight;        
+    }
+
+    create(name, x, y, width, height, options = {}) {
+        this._objects[name] = new EngineObject(name, x, y, width, height, options);
+
+        if (!this._objects[name].options.disabledEvents) {
+        }
+    }
+
+    add(object) {
+        
+    }
+}
